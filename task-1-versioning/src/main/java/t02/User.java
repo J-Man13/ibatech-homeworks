b0398d6d0cad0dde7f9255b04b1c61ff79affa44
jafarjafarov <cafar.9r4@gmail.com>
2022-06-04 11:29:47 +0400

    Added tas1-versioning

---
+++ task-1-versioning/src/main/java/t02/User.java
@@ -0,0 +1,14 @@
+package t02;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+  Integer id;
+  String name;
+  String lastName;
+}
